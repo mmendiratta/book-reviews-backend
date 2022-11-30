64dc51866b43cfb945db80c3cba0264a6ab08904
mmendiratta <mannimendiratta@yahoo.com>
2022-11-29 20:26:37 -0600

    starter project initialize

---
+++ src/main/java/com/mendiratta/bookreviews/BookreviewsApplication.java
@@ -0,0 +1,13 @@
+package com.mendiratta.bookreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreviewsApplication.class, args);
+	}
+
+}
